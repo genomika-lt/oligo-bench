13a6599f12bb8c5b51a2261b606924c817793e94
Dainius <kirsnauskas1@gmail.com>
2024-08-19 14:31:06 +0300

    fix pylint issues

--- workflow/scripts/count_mism_bam.py
+++ workflow/scripts/count_mism_bam.py
@@ -1,53 +1,64 @@
-import sys
+"""Describes function for counting mismatches in bam file"""
+
+
+import logging
+
 import pysam
 import pandas as pd
 
-# logging
-sys.stderr = open(snakemake.log[0], "w")
+from snakemake.script import snakemake
 
-def count_mism_bam(bam, out, threads):
-    
-    pysam.index(bam)
-    
-    inp = pysam.AlignmentFile(bam, 'rb', threads=threads)
-    
-    # init a dict to count mismaches
+
+# logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=snakemake.log[0],
+                    filemode='w',
+                    encoding='utf-8',
+                    level=logging.INFO)
+
+
+def count_mism_bam(bam_file, output_file, threads):
+    """
+    Counts mismatches in bam and writes output to out
+    :param str bam_file: Input bam file
+    :param str output_file: Output file to save data
+    :param int threads: Number of threads to use
+    :rtype: None
+    """
+
+    pysam.index(bam_file)
+
+    inp = pysam.AlignmentFile(bam_file, 'rb', threads=threads)
+
+    # init a dict to count mismatches
     mism_counts = {
         'NM0': 0, 'NM1': 0, 'NM2': 0,
         'NM3': 0, 'NM4': 0, '>NM5': 0
     }
-    
+
     # parse the BAM file
     for read in inp.fetch():
         # use primary reads
         if not read.is_supplementary and not read.is_secondary:
-            nm = read.get_tag("NM")
-            if nm == 0:
-                mism_counts['NM0'] += 1
-            elif nm == 1:
-                mism_counts['NM1'] += 1
-            elif nm == 2:
-                mism_counts['NM2'] += 1
-            elif nm == 3:
-                mism_counts['NM3'] += 1
-            elif nm == 4:
-                mism_counts['NM4'] += 1
+            number_of_mismatches = read.get_tag("NM")
+            if number_of_mismatches < 5:
+                mism_counts['NM' + str(number_of_mismatches)] += 1
             else:
                 mism_counts['>NM5'] += 1
-    
+
     inp.close()
-    
+
     # flatten
-    mism_df = pd.DataFrame(
-        list(mism_counts.items()), columns=['mism', 'count']
-    )
+    mism_df = pd.DataFrame(list(mism_counts.items()),
+                           columns=['mism', 'count'])
 
     # write output
-    mism_df.to_csv(out, header=False, index=False)
+    mism_df.to_csv(output_file, header=False, index=False)
 
 
-count_mism_bam(
-    bam = snakemake.input[0],
-    out = snakemake.output[0],
-    threads = snakemake.threads
-)
+try:
+    count_mism_bam(bam_file=snakemake.input[0], output_file=snakemake.output[0],
+                   threads=snakemake.threads)
+except Exception as e:
+    logger.exception(e)
+    raise e
--- workflow/scripts/parse_samtools_stats.py
+++ workflow/scripts/parse_samtools_stats.py
@@ -1,65 +1,60 @@
+"""Describes function for parsing samtools statistics"""
+
+import logging
+
 import pandas as pd
-import sys
+
+from snakemake.script import snakemake
+
 
 # logging
-sys.stderr = open(snakemake.log[0], "w")
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=snakemake.log[0],
+                    filemode='w',
+                    encoding='utf-8',
+                    level=logging.INFO)
 
 
-def parse_samtools_stats(inp, samples, out):
-    
+def parse_samtools_stats(input_file, samples, output_file):
+    """
+    Parses samtools statistics input file and save results to output_file.
+    :param str input_file: Input file with data to parse
+    :param str samples: Samples
+    :param list[str] output_file: List of output file for mq, in, dl and co statistics
+    :rtype: None
+    """
+
     mq_lines = []
     id_lines = []
     co_lines = []
-    
-    with open(inp, 'r') as f:
-        for line in f:
+
+    with open(input_file, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            values = line.strip().split('\t')
             if line.startswith("MAPQ"):
-                mq_lines.append(line)
+                mq_lines.append(values)
             elif line.startswith("ID"):
-                id_lines.append(line)
+                id_lines.append(values)
             elif line.startswith("COV"):
-                co_lines.append(line)
+                co_lines.append(values)
+            else:
+                raise ValueError("Unexpected line in samtools statistics file")
 
-    mq_df = pd.DataFrame(
-        [x.strip().split('\t') for x in mq_lines],
-        columns=["Type", "MAPQ", samples]
-    ).iloc[:, [2]].T
-    
-    in_df = pd.DataFrame(
-        [x.strip().split('\t') for x in id_lines],
-        columns=["Type", "Length", samples, "Num_Del"]
-    ).iloc[:, [2]].T
-    
-    dl_df = pd.DataFrame(
-        [x.strip().split('\t') for x in id_lines],
-        columns=["Type", "Length", "Num_Ins", samples]
-    ).iloc[:, [3]].T
-        
-    co_df = pd.DataFrame(
-        [x.strip().split('\t') for x in co_lines],
-        columns=["Type", "Range", "Depth", samples]
-    ).iloc[:, [3]].T
+    mq_df = pd.DataFrame(mq_lines, columns=["Type", "MAPQ", samples]).iloc[:, [2]].T
+    in_df = pd.DataFrame(id_lines, columns=["Type", "Length", samples, "Num_Del"]).iloc[:, [2]].T
+    dl_df = pd.DataFrame(id_lines, columns=["Type", "Length", "Num_Ins", samples]).iloc[:, [3]].T
+    co_df = pd.DataFrame(co_lines, columns=["Type", "Range", "Depth", samples]).iloc[:, [3]].T
 
-    mq_df.to_csv(
-        out[0],
-        header=False
-    )
-    in_df.to_csv(
-        out[1],
-        header=False
-    )
-    dl_df.to_csv(
-        out[2],
-        header=False
-    )
-    co_df.to_csv(
-        out[3],
-        header=False
-    )
+    mq_df.to_csv(output_file[0], header=False)
+    in_df.to_csv(output_file[1], header=False)
+    dl_df.to_csv(output_file[2], header=False)
+    co_df.to_csv(output_file[3], header=False)
 
 
-parse_samtools_stats(
-    inp = snakemake.input[0],
-    samples = snakemake.params.samples,
-    out = snakemake.output
-)
+try:
+    parse_samtools_stats(input_file=snakemake.input[0],
+                         samples=snakemake.params.samples,
+                         output_file=snakemake.output)
+except Exception as e:
+    logger.exception(e)
+    raise e
