3e6e4f2036598487d717d884777c4248f4ab37cd
Dainius Kirsnauskas <dainius@genomika.lt>
2024-08-27 11:59:04 +0300

    add N50 table

--- workflow/scripts/statistics/N50.py
+++ workflow/scripts/statistics/N50.py
@@ -1,4 +1,4 @@
-"""Counts total number of reads"""
+"""Calculates N50 for each sample and saves to html table"""
 
 
 import logging
@@ -17,9 +17,25 @@ logging.basicConfig(filename=snakemake.log[0],
                     level=logging.INFO)
 
 
-def count_total_passed_reads(bam_files, output_file):
+def calculate_n50_for_numbers(list_of_lengths):
     """
-    Counts total number of reads across samples
+    Calculates N50 for list of lengths of sequences
+    :param list[int] list_of_lengths: list with all lengths of sequences
+    :return: Calculated N50
+    :rtype: int
+    """
+
+    total_sum_half = sum(list_of_lengths) / 2
+    for length in sorted(list_of_lengths, reverse=True):
+        total_sum_half -= length
+        if total_sum_half <= 0:
+            return length
+
+    return 0
+
+def calculate_n50_for_bam_files(bam_files, output_file):
+    """
+    Calculates N50 for each sample and saves to html table
     :param str bam_files: Folder with fastq reads
     :param str output_file: Output file to save data
     :rtype: None
@@ -27,14 +43,16 @@ def count_total_passed_reads(bam_files, output_file):
 
 
     header = ['Parameter']
-    values = ['Total reads']
+    values = ['N50']
 
     for file in bam_files:
         out = pysam.view('-o', 'out.sam', file)
-        count = len(out.split('\n')) - 1
+        list_of_lengths = [len(line.split('\t')[9]) for line in out.split('\n')[:-1]]
+
+        n50 = calculate_n50_for_numbers(list_of_lengths)
 
         header.append(file.split('/')[-1])
-        values.append(count)
+        values.append(n50)
 
     figure = go.Figure(data=[go.Table(header={'values': header},
                                       cells={'values': values})])
@@ -44,7 +62,7 @@ def count_total_passed_reads(bam_files, output_file):
 
 
 try:
-    count_total_passed_reads(bam_files=snakemake.input, output_file=snakemake.output[0])
+    calculate_n50_for_bam_files(bam_files=snakemake.input, output_file=snakemake.output[0])
 except Exception as e:
     logger.exception(e)
     raise e
