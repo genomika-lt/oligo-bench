3e90e5f781149cc87a95028cff62c62366c38213
Dainius Kirsnauskas <dainius@genomika.lt>
2024-11-06 17:09:45 +0200

    improve summary table

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -2,6 +2,7 @@
 
 
 import logging
+from collections.abc import Iterable
 from math import floor, log10
 
 from snakemake.script import snakemake
@@ -14,6 +15,8 @@ def snakemake_file_logger(func):
     :return: Wrapped function.
     :rtype: Callable
     """
+
+
     logging.basicConfig(filename=snakemake.log[0],
                         filemode='w',
                         encoding='utf-8',
@@ -37,6 +40,7 @@ def parse_sam_records(records):
     :param list[list[str]] records: Records from BAM file.
     :return: Parsed records.
     """
+
 
     parsed_records = []
     for record in records:
@@ -62,7 +66,7 @@ def parse_sam_records(records):
     return parsed_records
 
 
-def round_to_x_significant(number, x):
+def round_to_x_significant(number: int|float, x: int):
     """
     Rounds number to x significant digits.
     :param number: Number to round.
@@ -70,15 +74,17 @@ def round_to_x_significant(number, x):
     :return: Rounded number.
     """
 
+
     return round(number, x - 1 - floor(log10(abs(number))))
 
 
-def from_phred_to_char(value: int|list[int]) -> str:
+def from_phred_to_char(value: int|Iterable[int]) -> str:
     """
     Converts integer value to character or list of integers to list of characters.
     :param value: Phred value or list of phred values.
     :return: Character of list of characters.
     """
+
 
     if isinstance(value, int):
         return chr(value + 33)
@@ -86,14 +92,36 @@ def from_phred_to_char(value: int|list[int]) -> str:
     return ''.join([chr(i + 33) for i in value])
 
 
-def from_char_to_phred(value: str|list[str]) -> int|list[int]:
+def from_char_to_phred(value: str|Iterable[str]) -> int|list[int]:
     """
     Converts character to phred value or list of characters to list of values.
     :param value: Character of list of characters.
     :return: Phred value or list of phred values.
     """
 
+
     if len(value) == 1:
         return ord(value[0]) - 33
 
     return [ord(i) - 33 for i in value]
+
+
+def integer_to_human_readable(number: int,
+                             alphabet: list[str] | tuple[str] = ('', 'K', 'M', 'B', 'T')) -> str:
+    """
+    Converts integer value to human-readable string with using user specified alphabet.
+    :param number: Number to convert.
+    :param alphabet: Alphabet to use.
+    :return: Converted, readable value.
+    """
+
+
+    string_number = str(number)
+    number_of_digits = len(string_number)
+    suffix_index = (number_of_digits - 1) // 3
+
+    if suffix_index >= len(alphabet):
+        raise IndexError(f'Too small alphabet: {len(alphabet)},'
+                         f' was referring to element number {suffix_index}')
+
+    return string_number[:len(string_number) - suffix_index * 3] + alphabet[suffix_index]
