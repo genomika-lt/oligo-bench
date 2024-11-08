a3512cab062699007b8427df669b12cc17c06762
Dainius Kirsnauskas <dainius@genomika.lt>
2024-11-08 14:20:02 +0200

    refactor real histogram

--- workflow/scripts/statistics/overall/passed_read_length_histogram.py
+++ workflow/scripts/statistics/overall/passed_read_length_histogram.py
@@ -1,13 +1,11 @@
 """Plots passed reads length histogram"""
 
-
-import pysam
 import plotly.graph_objects as go
 
 from snakemake.script import snakemake
 
 # pylint: disable=import-error
-from scripts.utils import parse_sam_records, snakemake_file_logger
+from scripts.utils import parse_sam_records, snakemake_file_logger, parse_bam_file
 
 
 
@@ -24,19 +22,33 @@ def passed_read_length_histogram(bam_files, output_file):
     figure = go.Figure()
 
     for path_to_sample in bam_files:
-        out = pysam.view('-o', 'out.sam', path_to_sample)
-        records = [line.split() for line in out.split('\n')[:-1]]
-        parsed_records = parse_sam_records(records)
-        data = [len(record[10]) for record in parsed_records]
-        figure.add_trace(go.Histogram(x=data,
-                                      name=path_to_sample.split('/')[-1][7:-4]))
+        data = {}
+        number_of_reads = 0
+        for read in parse_bam_file(path_to_sample):
+            if len(read.qual) not in data:
+                data[len(read.qual)] = 1
+            else:
+                data[len(read.qual)] += 1
+            number_of_reads += 1
+
+        x = list(data.keys())
+        x.sort()
+
+        y = [data[key] for key in x]
+        y_max = max(y)
+        y_normalized = [i / y_max for i in y]
+
+        figure.add_trace(go.Scatter(y=y_normalized,
+                                    x=x,
+                                    name=path_to_sample.split('/')[-1][7:-4]))
+    figure.update_xaxes(type="log")
 
     figure.update_layout(title="Passed Read Length Histogram",
                          xaxis_title="Length",
                          yaxis_title="Number Of Reads",
                          legend_title="Samples")
     figure.update_layout(barmode='overlay')
-    figure.update_traces(opacity=0.5)
+    figure.update_traces(opacity=0.65)
 
     with open(output_file, 'w', encoding='utf-8') as g:
         g.write(figure.to_html(full_html=False, include_plotlyjs='cdn'))
