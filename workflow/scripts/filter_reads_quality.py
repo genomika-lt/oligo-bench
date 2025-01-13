a52d5882f022ea5b1045643ecc0b247fd17900c0
Akaud <akadlevchenko.com>
2025-01-13 17:02:54 +0200

    change filtering for mean quality to use qs tag values inside bam files

--- workflow/scripts/filter_reads_quality.py
+++ workflow/scripts/filter_reads_quality.py
@@ -23,7 +23,7 @@ def filter_basecalled(bam_file, output_file):
     # pylint: disable=no-member
     filtered = pysam.AlignmentFile(output_file, "wb", template=data, check_sq=False)
     for read in data:
-        if sum(read.query_qualities) / len(read.query_qualities) > int(snakemake.config["parameters"]["minimum_basecalling_quality"]["value"]):
+        if read.get_tag("qs") > int(snakemake.config["parameters"]["minimum_basecalling_quality"]["value"]):
             filtered.write(read)
 
 
